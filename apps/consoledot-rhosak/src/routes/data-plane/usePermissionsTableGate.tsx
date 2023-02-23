7059701e9d24b9196bcc7d3b2fddcd6c78682ca2
suyash-naithani <suyash.naithani@gmail.com>
2023-02-23 16:07:20 +0530

    chore:Remove suspense while fetching data

--- apps/consoledot-rhosak/src/routes/data-plane/usePermissionsTableGate.tsx
+++ apps/consoledot-rhosak/src/routes/data-plane/usePermissionsTableGate.tsx
@@ -1,4 +1,8 @@
-import { usePaginationSearchParams } from "@rhoas/app-services-ui-components";
+import type { Account } from "@rhoas/app-services-ui-components";
+import {
+  PrincipalType,
+  usePaginationSearchParams,
+} from "@rhoas/app-services-ui-components";
 import { useAcls, useServiceAccounts, useUserAccounts } from "consoledot-api";
 import { useRouteMatch } from "react-router-dom";
 import { DataPlanePermissionsTableRoutePath } from "./routesConsts";
@@ -11,36 +15,51 @@ export function usePermissionsTableGate() {
   if (!match) {
     throw Error("usePermissionsTableGate used outside the expected route");
   }
-  const { data: acls } = useAcls(
-    {
-      id: instance.id,
-      adminUrl: instance.adminUrl,
-      page,
-      perPage,
-    },
-    true
+  const { data: acls } = useAcls({
+    id: instance.id,
+    adminUrl: instance.adminUrl,
+    page,
+    perPage,
+  });
+
+  const { data: accounts } = useUserAccounts({
+    id: instance.id,
+    adminUrl: instance.adminUrl,
+  });
+
+  const { data: serviceAccounts } = useServiceAccounts({
+    id: instance.id,
+    adminUrl: instance.adminUrl,
+  });
+
+  const userAccounts: Account[] | undefined = accounts?.accounts.map(
+    (userAccount) => {
+      return {
+        id: userAccount.username,
+        displayName: userAccount.displayName,
+        email: userAccount.email,
+        principalType: PrincipalType.UserAccount,
+      };
+    }
   );
 
-  const { data: accounts } = useUserAccounts(
-    {
-      id: instance.id,
-      adminUrl: instance.adminUrl,
-    },
-    true
-  );
+  const serviceAccountList: Account[] | undefined =
+    serviceAccounts?.serviceAccounts.map((serviceAccount) => {
+      return {
+        id: serviceAccount.id,
+        displayName: serviceAccount.displayName,
+        principalType: PrincipalType.ServiceAccount,
+      };
+    });
 
-  const { data: serviceAccounts } = useServiceAccounts(
-    {
-      id: instance.id,
-      adminUrl: instance.adminUrl,
-    },
-    true
-  );
+  const allAccounts =
+    serviceAccountList != undefined && userAccounts != undefined
+      ? [...serviceAccountList, ...userAccounts]
+      :undefined
 
   return {
     instance,
-    acls: acls as NonNullable<typeof acls>,
-    accounts: accounts as NonNullable<typeof accounts>,
-    serviceAccounts: serviceAccounts as NonNullable<typeof serviceAccounts>,
+    acls: acls,
+    allAccounts: allAccounts,
   };
 }
