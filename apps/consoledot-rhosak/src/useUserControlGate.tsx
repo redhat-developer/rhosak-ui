2d350a4bb56c2d00a405ade7021419283ae3b2cc
Riccardo Forina <rforina@redhat.com>
2023-03-08 10:33:08 +0100

    chore: update deps and remove @redhat-cloud-services/frontend-components from the api deps

--- apps/consoledot-rhosak/src/useUserControlGate.tsx
+++ apps/consoledot-rhosak/src/useUserControlGate.tsx
@@ -1,6 +1,18 @@
+import { useChrome } from "@redhat-cloud-services/frontend-components/useChrome";
 import { useUser } from "consoledot-api";
+import { useCallback } from "react";
 
 export function useUserControlGate() {
-  const { data: userData } = useUser(true);
+  const { auth } = useChrome();
+  const getUser = useCallback(async ()=> {
+    const user = await auth.getUser();
+    const username = user?.identity?.user?.username;
+    const isOrgAdmin = user?.identity?.user?.is_org_admin;
+    return {
+      username,
+      isOrgAdmin,
+    }
+  }, [auth]);
+  const { data: userData } = useUser({ getUser, suspense: true });
   return { userData: userData as NonNullable<typeof userData> };
 }
