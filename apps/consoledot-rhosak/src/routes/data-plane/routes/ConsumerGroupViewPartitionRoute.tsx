8f5e44579d603bef90c983501121864814431c77
hemahg <hhg@redhat.com>
2023-03-14 14:05:30 +0530

    fix format
    Signed-off-by: hemahg <hhg@redhat.com>

--- apps/consoledot-rhosak/src/routes/data-plane/routes/ConsumerGroupViewPartitionRoute.tsx
+++ apps/consoledot-rhosak/src/routes/data-plane/routes/ConsumerGroupViewPartitionRoute.tsx
@@ -13,60 +13,60 @@ export const ConsumerGroupViewPartitionRoute: VoidFunctionComponent<
   viewPartitionConsumerGroupHref,
   instancesHref,
 }) => {
-    const { instance, consumerGroup } = useConsumerGroupGate();
+  const { instance, consumerGroup } = useConsumerGroupGate();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const history = useHistory();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const history = useHistory();
 
-    const onCancel = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(instanceConsumerGroupsHref(instance.id));
-    }, [history, instance.id, instanceConsumerGroupsHref]);
+  const onCancel = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(instanceConsumerGroupsHref(instance.id));
+  }, [history, instance.id, instanceConsumerGroupsHref]);
 
-    const onDelete = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(
-        `${viewPartitionConsumerGroupHref(
-          instance.id,
-          consumerGroup.name
-        )}/delete`
-      );
-    }, [
-      history,
-      instance.id,
-      consumerGroup.name,
-      viewPartitionConsumerGroupHref,
-    ]);
-
-    const onClickResetOffset = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(
-        `${viewPartitionConsumerGroupHref(
-          instance.id,
-          consumerGroup.name
-        )}/reset-offset`
-      );
-    }, [
-      history,
-      instance.id,
-      consumerGroup.name,
-      viewPartitionConsumerGroupHref,
-    ]);
-
-    return (
-      <ConsumerGroupDrawer
-        consumerGroupByTopic={false}
-        state={consumerGroup.state}
-        activeMembers={consumerGroup.activeConsumers!}
-        partitionsWithLag={consumerGroup.laggingPartitions!}
-        consumers={consumerGroup.consumers}
-        groupId={consumerGroup.name}
-        onSelectDeleteConsumerGroup={onDelete}
-        onSelectResetOffsetConsumerGroup={onClickResetOffset}
-        isExpanded={true}
-        onClickClose={onCancel}
-      >
-        <ConsumerGroupsRoute instancesHref={instancesHref} />
-      </ConsumerGroupDrawer>
+  const onDelete = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(
+      `${viewPartitionConsumerGroupHref(
+        instance.id,
+        consumerGroup.name
+      )}/delete`
     );
-  };
+  }, [
+    history,
+    instance.id,
+    consumerGroup.name,
+    viewPartitionConsumerGroupHref,
+  ]);
+
+  const onClickResetOffset = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(
+      `${viewPartitionConsumerGroupHref(
+        instance.id,
+        consumerGroup.name
+      )}/reset-offset`
+    );
+  }, [
+    history,
+    instance.id,
+    consumerGroup.name,
+    viewPartitionConsumerGroupHref,
+  ]);
+
+  return (
+    <ConsumerGroupDrawer
+      consumerGroupByTopic={false}
+      state={consumerGroup.state}
+      activeMembers={consumerGroup.activeConsumers!}
+      partitionsWithLag={consumerGroup.laggingPartitions!}
+      consumers={consumerGroup.consumers}
+      groupId={consumerGroup.name}
+      onSelectDeleteConsumerGroup={onDelete}
+      onSelectResetOffsetConsumerGroup={onClickResetOffset}
+      isExpanded={true}
+      onClickClose={onCancel}
+    >
+      <ConsumerGroupsRoute instancesHref={instancesHref} />
+    </ConsumerGroupDrawer>
+  );
+};
--- apps/consoledot-rhosak/src/routes/data-plane/routes/TopicConsumerGroupViewPartitionRoute.tsx
+++ apps/consoledot-rhosak/src/routes/data-plane/routes/TopicConsumerGroupViewPartitionRoute.tsx
@@ -15,68 +15,68 @@ export const TopicConsumerGroupViewPartitionRoute: VoidFunctionComponent<
   instanceDetailsHref,
   instanceTopicsHref,
 }) => {
-    const { instance, topic, consumerGroup } = useTopicConsumerGroupGate();
+  const { instance, topic, consumerGroup } = useTopicConsumerGroupGate();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const history = useHistory();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const history = useHistory();
 
-    const onCancel = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(instanceTopicConsumerGroupsHref(instance.id, topic.name));
-    }, [history, instance.id, topic.name, instanceTopicConsumerGroupsHref]);
+  const onCancel = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(instanceTopicConsumerGroupsHref(instance.id, topic.name));
+  }, [history, instance.id, topic.name, instanceTopicConsumerGroupsHref]);
 
-    const onDelete = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(
-        `${viewTopicPartitionConsumerGroupHref(
-          instance.id,
-          topic.name,
-          consumerGroup.name
-        )}/delete`
-      );
-    }, [
-      history,
-      instance.id,
-      consumerGroup.name,
-      topic.name,
-      viewTopicPartitionConsumerGroupHref,
-    ]);
-
-    const onClickResetOffset = useCallback(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-      history.push(
-        `${viewTopicPartitionConsumerGroupHref(
-          instance.id,
-          topic.name,
-          consumerGroup.name
-        )}/reset-offset`
-      );
-    }, [
-      history,
-      instance.id,
-      topic.name,
-      consumerGroup.name,
-      viewTopicPartitionConsumerGroupHref,
-    ]);
-
-    return (
-      <ConsumerGroupDrawer
-        consumerGroupByTopic={false}
-        state={consumerGroup.state}
-        activeMembers={consumerGroup.activeConsumers!}
-        partitionsWithLag={consumerGroup.laggingPartitions!}
-        consumers={consumerGroup.consumers}
-        groupId={consumerGroup.name}
-        onSelectDeleteConsumerGroup={onDelete}
-        onSelectResetOffsetConsumerGroup={onClickResetOffset}
-        isExpanded={true}
-        onClickClose={onCancel}
-      >
-        <TopicConsumerGroupsRoute
-          instancesHref={instancesHref}
-          instanceDetailsHref={instanceDetailsHref}
-          instanceTopicsHref={instanceTopicsHref}
-        />
-      </ConsumerGroupDrawer>
+  const onDelete = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(
+      `${viewTopicPartitionConsumerGroupHref(
+        instance.id,
+        topic.name,
+        consumerGroup.name
+      )}/delete`
     );
-  };
+  }, [
+    history,
+    instance.id,
+    consumerGroup.name,
+    topic.name,
+    viewTopicPartitionConsumerGroupHref,
+  ]);
+
+  const onClickResetOffset = useCallback(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    history.push(
+      `${viewTopicPartitionConsumerGroupHref(
+        instance.id,
+        topic.name,
+        consumerGroup.name
+      )}/reset-offset`
+    );
+  }, [
+    history,
+    instance.id,
+    topic.name,
+    consumerGroup.name,
+    viewTopicPartitionConsumerGroupHref,
+  ]);
+
+  return (
+    <ConsumerGroupDrawer
+      consumerGroupByTopic={false}
+      state={consumerGroup.state}
+      activeMembers={consumerGroup.activeConsumers!}
+      partitionsWithLag={consumerGroup.laggingPartitions!}
+      consumers={consumerGroup.consumers}
+      groupId={consumerGroup.name}
+      onSelectDeleteConsumerGroup={onDelete}
+      onSelectResetOffsetConsumerGroup={onClickResetOffset}
+      isExpanded={true}
+      onClickClose={onCancel}
+    >
+      <TopicConsumerGroupsRoute
+        instancesHref={instancesHref}
+        instanceDetailsHref={instanceDetailsHref}
+        instanceTopicsHref={instanceTopicsHref}
+      />
+    </ConsumerGroupDrawer>
+  );
+};
