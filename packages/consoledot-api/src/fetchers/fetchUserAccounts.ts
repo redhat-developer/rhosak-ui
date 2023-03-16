ec51c6174e84cbc9fac358c67c4cb6a9326600ce
suyash-naithani <suyash.naithani@gmail.com>
2023-03-16 16:35:43 +0530

    chore: catch api error while fetching user accounts

--- packages/consoledot-api/src/fetchers/fetchUserAccounts.ts
+++ packages/consoledot-api/src/fetchers/fetchUserAccounts.ts
@@ -11,21 +11,25 @@ export type FetchUserAccountsParams = {
 export async function fetchUserAccounts({
   getUserAccounts,
 }: FetchUserAccountsParams): Promise<{
-  accounts: UserAccount[];
+  accounts: UserAccount[] | [];
   count: number;
 }> {
-  const response = await getUserAccounts(-1);
-  const accounts = response.data.data.map<UserAccount>((p) => {
-    const fullObject = p as Principal;
-    return {
-      username: fullObject.username,
-      displayName: `${fullObject.first_name || ""} ${
-        fullObject.last_name || ""
-      }`.trim(),
-      email: fullObject.email,
-      isOrgAdmin: fullObject.is_org_admin || false,
-    };
-  });
-  const count = accounts.length;
-  return { count, accounts };
+  try {
+    const response = await getUserAccounts(-1);
+    const accounts = response.data.data.map<UserAccount>((p) => {
+      const fullObject = p as Principal;
+      return {
+        username: fullObject.username,
+        displayName: `${fullObject.first_name || ""} ${
+          fullObject.last_name || ""
+        }`.trim(),
+        email: fullObject.email,
+        isOrgAdmin: fullObject.is_org_admin || false,
+      };
+    });
+    const count = accounts.length;
+    return { count, accounts };
+  } catch {
+    return { accounts: [], count: 0 };
+  }
 }
