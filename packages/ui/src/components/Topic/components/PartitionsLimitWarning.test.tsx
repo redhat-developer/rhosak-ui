0cbffebee0bb1aba692d9c3a8728344bd4c08138
suyash-naithani <suyash.naithani@gmail.com>
2023-03-17 12:44:24 +0530

    Add story and test

--- packages/ui/src/components/Topic/components/PartitionsLimitWarning.test.tsx
+++ packages/ui/src/components/Topic/components/PartitionsLimitWarning.test.tsx
@@ -3,14 +3,14 @@ import { composeStories } from "@storybook/testing-react";
 import { render, waitForI18n } from "../../../test-utils";
 import * as stories from "./PartitionsLimitWarning.stories";
 
-const { EmptyState } = composeStories(stories);
+const { EditTopicWarning, CreateTopicWarning } = composeStories(stories);
 
 describe("Partitions warning", () => {
   it("should render a partitions warning modal", async () => {
     const onSave = jest.fn();
     const isModalOpen = jest.fn();
     const comp = render(
-      <EmptyState onSave={onSave} setIsModalOpen={isModalOpen} />
+      <CreateTopicWarning onSave={onSave} setIsModalOpen={isModalOpen} />
     );
     await waitForI18n(comp);
     expect(
@@ -19,6 +19,28 @@ describe("Partitions warning", () => {
     expect(
       comp.getByText(
         "Since this Kafka Instance has already reached its maximum partition limit, increasing the number of partitions might result in degraded performance."
+      )
+    ).toBeInTheDocument();
+    expect(comp.getByText("No, return to form")).toBeInTheDocument();
+    expect(comp.getByText("Yes")).toBeInTheDocument();
+    userEvent.click(comp.getByText("No, return to form"));
+    expect(isModalOpen).toBeCalledTimes(1);
+    userEvent.click(comp.getByText("Yes"));
+    expect(onSave).toBeCalledTimes(1);
+  });
+  it("should render a partitions warning modal when editing a topic", async () => {
+    const onSave = jest.fn();
+    const isModalOpen = jest.fn();
+    const comp = render(
+      <EditTopicWarning onSave={onSave} setIsModalOpen={isModalOpen} />
+    );
+    await waitForI18n(comp);
+    expect(
+      comp.getByText("Increase the number of partitions?")
+    ).toBeInTheDocument();
+    expect(
+      comp.getByText(
+        "Messages might have the same key from two different partitions, which can potentially break the message ordering guarantees that apply to a single partition."
       )
     ).toBeInTheDocument();
     expect(comp.getByText("No, return to form")).toBeInTheDocument();
