fd4ed0326a3299b35bd65947c25e10afc3dfdcc6
hemahg <hhg@redhat.com>
2023-03-23 15:28:14 +0530

    remove the if else statement
    Signed-off-by: hemahg <hhg@redhat.com>

--- apps/consoledot-rhosak/src/routes/data-plane/routes/TopicCreateRoute.tsx
+++ apps/consoledot-rhosak/src/routes/data-plane/routes/TopicCreateRoute.tsx
@@ -117,10 +117,7 @@ export const TopicCreateRoute: VoidFunctionComponent<
         };
   }, [instance.plan]);
 
-  const isSaving = (() => {
-    if (createTopic.isLoading) return true;
-    else return false;
-  })();
+  const isSaving = createTopic.isLoading;
 
   return (
     <CreateTopic
